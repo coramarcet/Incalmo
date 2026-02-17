f9e6e7ab1d666a48b157db49b816e729ae31a1b9
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-02-17 14:27:30 -0500

    llm strategy name fix

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import asyncio
 from incalmo.incalmo_runner import run_incalmo_strategy
 from incalmo.core.services.config_service import ConfigService
 from incalmo.c2server.state_store import StateStore
+from config.attacker_config import StateMachineStrategy
 
 
 async def main():
     print("Starting Incalmo C2 server using configservice")
     StateStore.initialize()  # Initialize the state store (reset the database)
     config = ConfigService().get_config()
-    await run_incalmo_strategy(config, task_id=config.strategy.name)
+    task_id = (
+        config.strategy.name
+        if isinstance(config.strategy, StateMachineStrategy)
+        else config.strategy.planning_llm
+    )
+    await run_incalmo_strategy(config, task_id=task_id)
 
 
 if __name__ == "__main__":
